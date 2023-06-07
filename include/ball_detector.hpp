182b924d7c6109ef46125f0e9f1714f0942a4f55
Gabriel S.F <gsfernan.w@gmail.com>
2023-06-07 23:21:20 +0700

    init commit

---
+++ include/ball_detector.hpp
@@ -0,0 +1,82 @@
+/**
+ * @file ball_detector.hpp
+ * @author ALFAROBI V13
+ * @brief Base class for ball detector
+ * @version 0.1
+ * @date 2023-06-07
+ * 
+ * 
+*/
+
+#pragma once
+
+#include <image_transport/image_transport.h>
+#include <opencv2/opencv.hpp>
+
+namespace alfarobi::vision
+{
+    /**
+     * @brief Base class for ball detector
+     * 
+     */
+    class BallDetector
+    {
+        protected:
+            /**
+             * @brief Ros node handle.
+             * 
+             */
+            ros::NodeHandle nh_;
+
+            /**
+             * @brief Main frame from the image input.
+             * 
+             */
+            cv::Mat main_frame;
+
+            /**
+             * @brief Publish ball coordinate.
+             * 
+             * @param x x coordinate of ball
+             * @param y y coordinate of ball
+             */
+            virtual void publish_coordinate(int x, int y) const final;
+
+            /**
+             * @brief Ball not found.
+             * 
+             */
+            virtual void publish_not_found() const final;
+
+            /**
+             * @brief This method will only be called once.
+             * 
+             */
+            virtual void setup();
+
+            /**
+             * @brief This method will be called in the loop.
+             * 
+             */
+            virtual void update();
+
+        private:
+            image_transport::ImageTransport it_;
+            image_transport::Subscriber sub_;
+
+            ros::Publisher pub_;
+
+            void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+            
+
+        public:
+            BallDetector();
+            ~BallDetector();
+
+            /**
+             * @brief Main loop method.
+             * 
+             */
+            virtual void process() final;
+    };
+}
---
+++ src/ball_detector.cpp
@@ -0,0 +1,75 @@
+#include <ros/ros.h>
+#include <yaml-cpp/yaml.h>
+#include <ball_detector.hpp>
+#include <opencv2/opencv.hpp>
+#include <geometry_msgs/Point.h>
+#include <cv_bridge/cv_bridge.h>
+
+#define TOPIC "Topic Name Config"
+#define CONFIG_PATH
+
+namespace av = alfarobi::vision;
+
+static YAML::Node *config = new YAML::Node(YAML::LoadFile(CONFIG_PATH));
+static int frame_height;
+static int frame_width;
+
+av::BallDetector::BallDetector() :
+it_(nh_),
+sub_(it_.subscribe((*config)[TOPIC]["image input"].as<std::string>(), 1, &av::BallDetector::imageCallback,this)),
+pub_(nh_.advertise<geometry_msgs::Point>((*config)[TOPIC]["ball coordinate"].as<std::string>(), 1000))
+{
+    delete config;
+}
+
+av::BallDetector::~BallDetector(){}
+
+void av::BallDetector::imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    try
+    {
+        main_frame = (cv_bridge::toCvShare(msg, "bgr8")->image).clone();
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    }
+}
+
+void av::BallDetector::publish_coordinate(int x, int y) const
+{
+    geometry_msgs::Point p;
+    if(x < 0 || x > frame_height) throw("argument outside the range!");
+    if(y < 0 || y > frame_height) throw("argument outside the range!");
+
+    p.x = x;
+    p.y = y;
+    p.z = -1;
+
+    pub_.publish(p);
+}
+
+void av::BallDetector::publish_not_found() const
+{
+    geometry_msgs::Point p;
+
+    p.x = -1;
+    p.y = -1;
+    p.z = -1;
+
+    pub_.publish(p);
+}
+
+void av::BallDetector::setup(){}
+void av::BallDetector::update(){}
+
+void av::BallDetector::process()
+{
+    av::BallDetector::setup();
+
+    while(ros::ok())
+    { 
+        if( !(main_frame.empty()) ) this->update();
+        ros::spinOnce();
+    }
+}
